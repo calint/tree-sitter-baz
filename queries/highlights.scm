cad43d2706b54aaebf469b01e6db9cc34f893512
calint <calin.tenitchi@gmail.com>
2025-10-29 11:29:27 +0700

    .

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,5 +1,3 @@
-; highlights.scm
-
 "(" @comment
 ")" @comment
 "[" @comment
@@ -7,7 +5,6 @@
 "{" @comment
 "}" @comment
 
-; Keywords
 (func_keyword) @keyword.function
 (type_keyword) @keyword.type
 (field_keyword) @keyword
@@ -23,7 +20,6 @@
 (and_keyword) @keyword.operator
 (or_keyword) @keyword.operator
 
-; Types
 (bool_type) @type
 (i8_type) @type
 (i16_type) @type
@@ -32,45 +28,35 @@
 (sized_array_type) @type
 (unsized_array_type) @type
 
-; Identifiers
 (identifier) @variable
 (function_definition name: (identifier) @function)
 (type_definition name: (identifier) @type.definition)
 (member_field name: (identifier) @variable.member)
-(member_field ":" (_)? @type)
+(member_field ":" type: (_) @type)
 (parameter name: (identifier) @variable.parameter)
 (return_annotation name: (identifier) @variable.parameter)
 
-; Function parameters type
 (parameter
   name: (identifier) @variable.parameter
   type: (identifier)? @type)
 
-; Variable declaration
 (variable_declaration
   destination: (identifier) @variable
   type: (identifier)? @type
   initializer: (_)? @variable)
 
-; Sized array types
-(sized_array_type (identifier) @type)
+(sized_array_type type: (identifier) @type)
 
-; Unsized array types
-(unsized_array_type
-  (identifier) @type)
+(unsized_array_type type: (identifier) @type)
 
-; Function calls
 (function_call function: (identifier) @function.call)
 
-; Literals
 (string_literal) @string
 (number_literal) @number
 (boolean_literal) @boolean
 
-; Comments
 (comment) @comment
 
-; Operators
 (comparison_operator) @operator
 (unary_expression operator: ["-" "~"] @operator)
 (multiplicative_expression operator: ["*" "/" "%"] @operator)
